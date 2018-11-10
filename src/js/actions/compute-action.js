e305fb8f729ac2e617d7733e58336d7f6fcaebfe
Emanuele Falzone <emafalzo@gmail.com>
2018-11-10 17:28:27 +0100

    Cost per yaer

--- src/js/actions/compute-action.js
+++ src/js/actions/compute-action.js
@@ -24,10 +24,27 @@ Action.prototype.run = function (parameters, solve) { // add "onCancel" paramete
     */
     // THIS CAN BE REMOVED (BEGIN)
 
+    function convertToCost(kwhYear,hours){
+      var kwhDay = kwhYear/365;
+      var kw = kwhDay/12;
+
+      var chf = 0.23;
+
+      return {
+        costDay  : kw*hours*chf,
+        costYear : kw*hours*365*chf
+      }
+    }
+
+    this.collection.findById(localStorage.getItem('model')).then(function (item) {
+      localStorage.setItem('cost-year', Math.round(convertToCost(parseInt(item['consumptions']), parseInt(parameters['usage'])).costYear));
+    })
+
     localStorage.setItem('cost-day', 0);
     localStorage.setItem('cost-week', 0);
     localStorage.setItem('cost-year', 0);
-  
+
+
     solve({
         event: 'computation-done-event', // Done
         // event: 'computation-fail-event', // Fail
